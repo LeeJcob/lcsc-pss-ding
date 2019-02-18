c868cee8f358ac5d9a3d6049b0be374dcc03e420
LeeJcob <254981927@qq.com>
2019-02-18 23:51:33 +0800

    交通补贴提醒

--- lcsc-ding-service/src/main/java/com/lcsc/ding/job/DayWarningTimer.java
+++ lcsc-ding-service/src/main/java/com/lcsc/ding/job/DayWarningTimer.java
@@ -1,13 +1,16 @@
 package com.lcsc.ding.job;
 
 import com.dingtalk.api.response.OapiAttendanceListResponse;
+import com.lcsc.ding.core.constant.Constant;
 import com.lcsc.ding.core.util.DingUtil;
 import com.lcsc.ding.core.util.HolidayUtil;
 import org.apache.commons.collections.CollectionUtils;
+import org.joda.time.DateTime;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -91,6 +94,57 @@ public class DayWarningTimer {
                     if (isException) {
 
                         DingUtil.push(userId, "尊敬的用户你好，今日你忘记打下班卡了哦，请及时补卡");
+                    }
+                }
+
+            }
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+    }
+
+
+    @Scheduled(cron = "0 35 9 * * *")
+    public void sumbitSubsidyTimer() {
+
+        try {
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+
+            String offTime = "22:00:00";
+
+            Date userCheckTime = null;
+
+            //前一天
+            Date yesterday = new DateTime().minusDays(1).toDate();
+
+            boolean workDay = new HolidayUtil().isWorkDay(yesterday);
+
+            // 是工作日就开始核查打卡信息
+            if (workDay) {
+
+                Set<String> userIdList = DingUtil.getUserIdList();
+
+                for (String userId : userIdList) {
+
+                    OapiAttendanceListResponse attendance = DingUtil.getAttendanceByUserId(yesterday, yesterday, userId);
+                    List<OapiAttendanceListResponse.Recordresult> recordresult = attendance.getRecordresult();
+
+                    for (OapiAttendanceListResponse.Recordresult recordresult1 : recordresult) {
+
+
+                        if (Constant.CHECKTYPE_OFFDUTY.equals(recordresult1.getCheckType()) && Constant.TIMERESULT_NORMAL.equals(recordresult1.getTimeResult())) {
+
+                            userCheckTime = recordresult1.getUserCheckTime();
+
+                            if (simpleDateFormat.parse(simpleDateFormat.format(userCheckTime)).after(simpleDateFormat.parse(offTime))) {
+
+                                DingUtil.push(userId, "您昨晚辛苦加班到十点之后，若有打车，别忘了填报销单哦");
+                            }
+
+                        }
+
                     }
                 }
 
