1eb28e987b5a9570be3695c265abeb51aa0aacb3
LeeJcob <254981927@qq.com>
2019-04-27 21:12:33 +0800

    gradle打包配置

--- lcsc-ding-web/src/main/java/com/lcsc/ding/Application.java
+++ lcsc-ding-web/src/main/java/com/lcsc/ding/Application.java
@@ -3,6 +3,8 @@ package com.lcsc.ding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -10,8 +12,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableSwagger2
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
 }
